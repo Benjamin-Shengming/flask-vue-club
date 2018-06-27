7af8c217480100955811a26270b11b248bc0f5c1
Benjamin-Shengming <shmhu@hotmail.com>
2018-06-27 23:42:04 +1000

    update

--- apps/service_new.py
+++ apps/service_new.py
@@ -2,6 +2,7 @@
 from collections import OrderedDict
 from uuid import uuid1
 import dash
+import json
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Event, State, Input, Output
@@ -13,21 +14,16 @@ MAX_IMG_TXT = 10
 
 def generate_id(index):
     section_id = "img_section_{}".format(index)
-    delete_button_id = "service_new_del_{}".format(index)
     upload_id ='service_new_upload_{}'.format(index)
     img_id ="service_new_img_{}".format(index)
-    return section_id, delete_button_id, upload_id, img_id
+    txt_id = "service_new_txt_{}".format(index)
+    return section_id, upload_id, img_id, txt_id
 
 def generate_new_img(index):
-    section_id, delete_button_id, upload_id, img_id, = generate_id(index)
+    section_id, upload_id, img_id, txt_id = generate_id(index)
     img_section = html.Div(id = section_id, children=[
         html.Div(className="row align-items-center", children=[
-            html.Div(className="col-sm-2", children=[
-                html.Button("Delete",
-                            id=delete_button_id,
-                            className="btn btn-outline-danger")
-            ]),
-            html.Div(className="col-sm-10", children=[
+            html.Div(className="col-sm-12", children=[
                 dcc.Upload(
                     id=upload_id,
                     children=html.Div([
@@ -36,13 +32,11 @@ def generate_new_img(index):
                     ]),
                     style={
                         'width': '100%',
-                        'height': '60px',
-                        'lineHeight': '60px',
+                        'height': '100%',
                         'borderWidth': '1px',
                         'borderStyle': 'dashed',
                         'borderRadius': '5px',
                         'textAlign': 'center',
-                        'margin': '10px'
                     }
                 ),
             ])
@@ -54,6 +48,15 @@ def generate_new_img(index):
                              className="img-fluid"),
                 ]),
         ]),
+        html.Div(className="row", children=[
+                html.Div(className="col-sm-12", children=[
+                    dcc.Textarea(id=txt_id,
+                            placeholder="Please input picture description",
+                            style={'width':'100%'},
+                            className="form-control form-control-lg")
+                ]),
+        ]),
+        html.Hr()
     ])
     return img_section
 
@@ -69,7 +72,7 @@ layout = html.Div(children=[
     ]),
     html.Div(className="row", children=[
             html.Div(className="col-sm-12", children=[
-                dcc.Input(id="service-title",
+                dcc.Input(id="service_new_title",
                         placeholder="Please input title",
                         className="form-control form-control-lg")
             ])
@@ -81,7 +84,7 @@ layout = html.Div(children=[
     ]),
     html.Div(className="row", children=[
             html.Div(className="col-sm-12", children=[
-                dcc.Textarea(id="service-descption",
+                dcc.Textarea(id="service_new_description",
                         placeholder="Please input service description",
                         style={'width':'100%'},
                         className="form-control form-control-lg")
@@ -94,7 +97,7 @@ layout = html.Div(children=[
     ]),
     html.Div(className="row", children=[
             html.Div(className="col-sm-12", children=[
-                dcc.Input(id="service-price",
+                dcc.Input(id="service_new_price",
                         placeholder="Please input service price",
                         style={'width':'100%'},
                         type="number",
@@ -110,7 +113,7 @@ layout = html.Div(children=[
     ]),
     html.Div(className="row", children=[
             html.Div(className="col-sm-12", children=[
-                dcc.Input(id="service-discount",
+                dcc.Input(id="service_new_discount",
                         placeholder="Please input service price",
                         style={'width':'100%'},
                         type="number",
@@ -179,23 +182,12 @@ layout = html.Div(children=[
         ])
     ]),
     html.Hr(),
-    html.Div(className="row align-items-center", children=[
-            html.Div(className="col-sm-4", children=[
-                html.Button("Add Text",
-                            id= "service_new_button_add_text",
-                            className="btn btn-outline-primary")
-            ]),
-            html.Div(className="col-sm-4", children=[
-                html.Button("Add Image",
-                            id= "service_new_button_add_image",
-                            className="btn btn-outline-primary")
-            ]),
-            html.Div(className="col-sm-4", children=[
-                html.Button("Submit",
-                            id= "service_new_button_submit",
-                            className="btn btn-outline-primary")
-            ]),
-    ]),
+    html.A(className="float", children=[
+        html.Button("Submit",
+                    id="service_new_button_submit",
+                    n_clicks=0,
+                    className="btn btn-outline-primary")
+    ])
 ])
 
 
@@ -208,47 +200,24 @@ def preview_img(contents):
         else:
             return ""
 
-def delete_img(n_clicks, children):
-    if n_clicks <= 0:
-        return children
-    '''
-    children = img_text_children
-    delete_children = []
-    for child in children:
-        print(child)
-        row0 = child.children[0]
-        del_button = row0.children[0].children[0]
-        #upload_ctl = row0.children[1].children[0]
-        #row1 = child.children[1]
-        #img_ctrl = row1.children[0].children[0]
-        if del_button.id == delete_button_id:
-            delete_children.append(child)
-    for item in delete_children:
-        print(item)
-    '''
-    print(delete_button_id)
-    return []
 @app.callback(Output('service_new_label_message', 'children'),
               [Input('service_new_button_submit', 'n_clicks')],
               [State('service_new_imgs_and_texts', 'children')])
-def create_new_service(submit_btn, imgs_texts_children):
+def create_new_service(n_clicks, imgs_texts_children):
+    if n_clicks <= 0:
+        return ""
+
     for item in imgs_texts_children:
-        #print(item)
-        pass
+        print(json.dumps(item, indent=2))
     return len(imgs_texts_children)
 
 
 for i in range(MAX_IMG_TXT):
-    section_id, delete_button_id, upload_id, img_id, = generate_id(i)
-    app.callback(Output(section_id, 'children'),
-                [Input(delete_button_id, 'n_clicks')],
-                [State(section_id, 'children')])(delete_img)
+    section_id, upload_id, img_id, txt_id = generate_id(i)
     app.callback(Output(img_id, 'src'),
                 [Input(upload_id, 'contents')])(preview_img)
 
 app.callback(Output('major-img', 'src'),
              [Input('upload', 'contents')])(preview_img)
-
-
 
 
